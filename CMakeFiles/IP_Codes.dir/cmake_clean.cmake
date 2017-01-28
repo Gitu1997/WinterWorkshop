f3837d9a106e9159eae67d6fb10884d9a92072f6
gitu1997 <gitu_poke@yahoo.com>
2017-01-28 10:14:27 +0530

    Added Cmake Lsts

---
+++ CMakeFiles/IP_Codes.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/IP_Codes.dir/IP1.cpp.o"
+  "IP_Codes.pdb"
+  "IP_Codes"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/IP_Codes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
